f471d2e4ceb96842b1f632720bfc271fd401ebd2
Dmitry Andrianov <dima.andrianov@gmail.com>
2020-08-17 22:07:11 -0700

    Encrypt password with md5 and generate a unique username

--- register.php
+++ register.php
@@ -79,6 +79,24 @@
         if (strlen($password) > 30 || strlen($lname) < 5) {
             array_push($error_array, "Your password must be btwn 5 and 30 characters<br>");
         }
+
+        if (empty($error_array)) {
+            // encrypt password with md5
+            $password = md5($password);
+            
+            // generate username by concatenation first name and last name
+            $username = strtolower($fname . "_" . $lname);
+            $check_username_query = mysqli_query($con, "SELECT username FROM users WHERE username='$username");
+            
+            $i = 0;
+            // if username exists add number to username
+            while(mysqli_num_rows($check_username_query) != 0) {
+                $i++;
+                $username = $username . '_' . $i;
+                $check_username_query = mysqli_query($con, "SELECT username FROM users WHERE username='$username");
+            }
+
+        }
     }
 ?>
 
