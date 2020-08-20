0feba3b2845ab36a0f0cd9d8fabf6923962b332b
Dmitry Andrianov <dima.andrianov@gmail.com>
2020-08-19 21:19:52 -0700

    Add login form. Write logic for login handler

---
+++ includes/form_handlers/login_handler.php
@@ -0,0 +1,20 @@
+<?php 
+    if(isset($_POST['login_button'])) {
+
+        $email = filter_var($_POST['log_email'], FILTER_SANITIZE_EMAIL); // sanitize email
+        $_SESSION['log_email'] = $email;
+        $password = md5($_POST['log_password']);
+
+        $check_database_query = mysqli_query($con, "SELECT * FROM users WHERE email='$email' and password='$password'");
+        $check_login_query = mysqli_num_rows($check_database_query);
+
+        if ($check_login_query == 1) {
+            $row = mysqli_fetch_array($check_login_query);
+            $username = $row['username'];
+            $email = $row['email'];
+            $_SESSION['username'] = $username;
+            header("Location: index.php");
+            exit();
+        }
+    }
+?>
--- register.php
+++ register.php
@@ -1,6 +1,7 @@
 <?php 
     require 'config/config.php';
-    require 'includes/form_handlers/register_handler.php'
+    require 'includes/form_handlers/register_handler.php';
+    require 'includes/form_handlers/login_handler.php';
     
 ?>
 
@@ -12,6 +13,12 @@
     <title>Register</title>
 </head>
 <body>  
+
+<form action="register.php" method="POST">
+    <input type="email" name="log_email" placeholder="Email Address"><br>
+    <input type="password" name="log_password" placeholder="Password"><br>
+    <input type="submit" name="login_button" placeholder="Confirm Password" value="Login"><br>
+</form>
 
 <form action="register.php" method="POST">
     <input type="text" name="reg_fname" placeholder="First Name"
@@ -54,7 +61,7 @@
     else if(in_array("Your passwords don't match<br>", $error_array)) echo "Your passwords don't match<br>"; ?>
     <input type="password" name="reg_password2" placeholder="Confirm Password" required>
     <br>
-    <input type="submit" name="register_button" placeholder="Confirm Password" valcue="Register">
+    <input type="submit" name="register_button" value="Register">
     <br>
     <?php if(in_array("<span>You're all set!</span>", $error_array)) echo "<span>You're all set!</span>"; ?>
 </form>
