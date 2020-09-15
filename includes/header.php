9f79bece29c71cc3f35e6308679f13282b3e6557
Dmitry Andrianov <dima.andrianov@gmail.com>
2020-09-14 18:38:22 -0700

    Add nav bar. Display user name in the nav bar.

--- includes/header.php
+++ includes/header.php
@@ -3,6 +3,8 @@
 
    if(isset($_SESSION['username'])) {
        $userLoggedIn = $_SESSION['username'];
+       $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$userLoggedIn'");
+       $user = mysqli_fetch_array($user_details_query);
    } else {
        header('Location: register.php');
    }
@@ -12,7 +14,42 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Welcome to ...</title>
+    <script src="assets/js/bootstrap.js"></script>
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
-hi <?php $userLoggedIn ?>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">LOGO</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item nav-user-name">
+                    Hello <?php echo $user['first_name']; ?>!
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Messages</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Notifications</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Friends</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Settings</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
