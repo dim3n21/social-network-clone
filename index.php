947e59b1ae471e62581c481e8fa4efd294e19b7c
Dmitry Andrianov <dima.andrianov@gmail.com>
2020-09-14 17:54:56 -0700

    Create header page. Redirect users not logged in

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
-<?php 
-   require 'config/config.php';
+<?php
+include("includes/header.php");
+session_destroy();
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    Hello World!
+Hello World!
 </body>
 </html>
